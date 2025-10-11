d8a9f1f646af63335c310f172c145019ba7f8214
Dan Lemon <daniel.lemon@amazeelabs.com>
2025-10-11 09:23:21 +0200

    chore: fix

--- .lagoon/scripts/polydock_post_deploy.sh
+++ .lagoon/scripts/polydock_post_deploy.sh
@@ -14,7 +14,7 @@ POLYDOCK_APP_IMAGE_DB_FILENAME="/app/web/sites/default/files/polydock/db-image"
 mkdir -p $POLYDOCK_TMP
 
 if [ -z "POLYDOCK_APP_IMAGE_URL" ]; then
-	export POLYDOCK_APP_IMAGE_URL=$APP_IMAGE_URL_DEFAULT
+  export POLYDOCK_APP_IMAGE_URL=$APP_IMAGE_URL_DEFAULT
 fi;
 
 if [ ! -f "$LOCKFILE" ]; then
@@ -60,6 +60,7 @@ if [ ! -f "$LOCKFILE" ]; then
       echo "Updating $POLYDOCK_GENERATED_APP_ADMIN_USERNAME password";
       drush upwd "$POLYDOCK_GENERATED_APP_ADMIN_USERNAME" "$POLYDOCK_GENERATED_APP_ADMIN_PASSWORD"
     fi;
+
   fi;
 
   echo "Created $LOCKFILE to ensure we don't run more than once"
